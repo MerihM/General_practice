2d0c9fbb65ee93e1a4db7af789ea5f9185e9b522
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-16 15:40:41 +0200

    Find second largest number in array

--- Geeks_for_Geeks_practice/school_dif/p1/SecondLargest.cpp
+++ Geeks_for_Geeks_practice/school_dif/p1/SecondLargest.cpp
@@ -3,8 +3,38 @@
 
 using namespace std;
 
+int print2largest(int Arr[], int n)
+{
+    int largest, second;
+    if (n <= 1 || (n == 2 && Arr[0] == Arr[1]))
+        return -1;
+    if (Arr[0] != Arr[1])
+    {
+        largest = max(Arr[0], Arr[1]);
+        second = min(Arr[0], Arr[1]);
+    }
+    else
+    {
+        largest = Arr[0];
+        second = 0;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (Arr[i] > largest)
+        {
+            second = largest;
+            largest = Arr[i];
+        }
+        else if (Arr[i] < largest && Arr[i] > second)
+            second = Arr[i];
+    }
+    return second;
+}
 int main()
 {
-
+    int arr[] = {5, 10, 10, 10};
+    int al = sizeof(arr) / sizeof(arr[0]); // length calculation
+    // cout << "The length of the array is: " << al << endl;
+    cout << print2largest(arr, al) << endl;
     return 0;
 }
