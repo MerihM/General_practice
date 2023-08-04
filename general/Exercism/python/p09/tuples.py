b8d6ded615ae8e06698e05a93e742ec41c86cc7c
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-04 12:35:35 +0200

    Implement create_record function

--- general/Exercism/python/p09/tuples.py
+++ general/Exercism/python/p09/tuples.py
@@ -39,8 +39,9 @@ def create_record(azara_record, rui_record):
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
-
-    pass
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    return 'not a match'
 
 
 def clean_up(combined_record_group):
