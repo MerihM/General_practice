00d2dbd5e03f25ecb882c68184410642feaac7ed
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-04 12:10:52 +0200

    Implement perfect_score function

--- general/Exercism/python/p08/loops.py
+++ general/Exercism/python/p08/loops.py
@@ -81,4 +81,7 @@ def perfect_score(student_info):
     :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
     """
 
-    pass
+    for info in student_info:
+        if (info[1] == 100):
+            return info
+    return []
