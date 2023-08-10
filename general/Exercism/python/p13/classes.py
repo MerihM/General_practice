8c8f01f356eed7aa68337eb5fbbf67a90a026d83
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-10 23:36:31 +0200

    Code cleanup

--- general/Exercism/python/p13/classes.py
+++ general/Exercism/python/p13/classes.py
@@ -1,23 +1,4 @@
-"""Solution to Ellen's Alien Game exercise."""
-
-
 class Alien:
-    """Create an Alien object with location x_coordinate and y_coordinate.
-
-    Attributes
-    ----------
-    (class)total_aliens_created: int
-    x_coordinate: int - Position on the x-axis.
-    y_coordinate: int - Position on the y-axis.
-    health: int - Amount of health points.
-
-    Methods
-    -------
-    hit(): Decrement Alien health by one point.
-    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
-    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
-    collision_detection(other): Implementation TBD.
-    """
     total_aliens_created = 0
 
     def __init__(self, x_coordinate, y_coordinate):
@@ -39,9 +20,5 @@ class Alien:
     def collision_detection(self, obj):
         pass
 
-
-
-
-#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
 def new_aliens_collection(positions):
     return [Alien(x, y) for (x, y) in positions]
