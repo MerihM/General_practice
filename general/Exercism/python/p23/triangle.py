57859662894a2848d8b050a794e76e4bad08e68f
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-15 23:06:29 +0200

    Implement isTriangle function

---
+++ general/Exercism/python/p23/triangle.py
@@ -0,0 +1,17 @@
+def equilateral(sides):
+    pass
+
+
+def isosceles(sides):
+    pass
+
+
+def scalene(sides):
+    pass
+
+def isTriangle(sides):
+    a, b, c = sides
+    sidesLength = a > 0 and b > 0 and c > 0
+    twoSides = (a + b >= c) and (a + c >= b) and (b + c >= a)
+    return sidesLength and twoSides
+#triangle is if all sides are > 0 and sum of two sides is greater or equal than third
