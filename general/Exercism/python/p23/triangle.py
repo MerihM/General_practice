a91cf7d808d5967ae03ad2b9b29b73f0b7e75c9e
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-15 23:15:36 +0200

    Implement scalene function

--- general/Exercism/python/p23/triangle.py
+++ general/Exercism/python/p23/triangle.py
@@ -4,21 +4,20 @@ def equilateral(sides):
         return (a == b) and (a == c)
     return False
 
-
-
 def isosceles(sides):
     if isTriangle(sides):
         a, b, c = sides
         return (a == b) or (b == c) or (a == c)
     return False
 
-
 def scalene(sides):
-    pass
+    if isTriangle(sides):
+        a, b, c = sides
+        return (a != b) and (b != c) and (c != a)
+    return False
 
 def isTriangle(sides):
     a, b, c = sides
     sidesLength = a > 0 and b > 0 and c > 0
     twoSides = (a + b >= c) and (a + c >= b) and (b + c >= a)
     return sidesLength and twoSides
-#triangle is if all sides are > 0 and sum of two sides is greater or equal than third
