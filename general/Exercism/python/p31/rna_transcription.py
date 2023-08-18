93e6f5579f617612642232287d8b9cb5638fddf0
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-18 23:55:35 +0200

    Implement to_rna function

--- general/Exercism/python/p31/rna_transcription.py
+++ general/Exercism/python/p31/rna_transcription.py
@@ -1,2 +1,11 @@
 def to_rna(dna_strand):
-    pass
+    dna_rna_pairs = {
+        'G':'C',
+        'C':'G',
+        'T':'A',
+        'A':'U'
+    }
+    strand = ''
+    for i in dna_strand:
+        strand += dna_rna_pairs[i]
+    return strand
